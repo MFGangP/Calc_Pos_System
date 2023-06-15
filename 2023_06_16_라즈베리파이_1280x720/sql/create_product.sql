bbcbd6377a12350e0a5e5d89e257efb097f82267
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-06-15 23:37:43 +0900

    2023.06.16 파일 복사

---
+++ 2023_06_16_라즈베리파이_1280x720/sql/create_product.sql
@@ -0,0 +1,17 @@
+# CREATE
+CREATE TABLE products(
+  idx int NOT NULL AUTO_INCREMENT,
+  prdName varchar(15) NOT NULL,
+  prdPrice int NOT NULL,
+  PRIMARY KEY (`idx`)
+);
+
+# INSERT
+INSERT INTO calckiosk.products
+(idx,
+prdName,
+prdPrice)
+VALUES
+(idx,
+prdName,
+prdPrice);
