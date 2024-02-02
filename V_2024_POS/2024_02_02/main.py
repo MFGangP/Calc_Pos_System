94e6179fe4f1c165bf8839b4b7f3afb8b3b858b6
PARK SEONG HYEON <lovehyun95@gmail.com>
2024-02-02 22:40:48 +0900

    새로운 버전 제작 시작 - 세팅 끝

--- V_2024_POS/2024_02_02/main.py
+++ V_2024_POS/2024_02_02/main.py
@@ -1,1 +1,16 @@
 # pyqt6, ptside6 사용해서 재 제작
+
+import sys
+import os
+import pymysql
+from datetime import datetime
+
+os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
+
+# 행 열 갯수 정의
+conn = None
+widgets = None
+
+all_Row = 15
+all_Column = 7
+
