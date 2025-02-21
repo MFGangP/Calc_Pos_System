d99792aebb38f80c0e9ff3d3d4d46b7b64da1134
PARK SEONG HYEON <62039259+MFGangP@users.noreply.github.com>
2025-02-21 20:51:44 +0900

    Create calckiosk_new_20250221.sql

---
+++ sql/calckiosk_new_20250221.sql
@@ -0,0 +1,108 @@
+CREATE DATABASE  IF NOT EXISTS `calckiosk_new` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `calckiosk_new`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: calckiosk_new
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `orderitems`
+--
+
+DROP TABLE IF EXISTS `orderitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderitems` (
+  `oimIdx` int NOT NULL AUTO_INCREMENT,
+  `prdIdx` int DEFAULT NULL,
+  `ordIdx` int DEFAULT NULL,
+  `quantity` int NOT NULL,
+  `totalPrice` int NOT NULL,
+  PRIMARY KEY (`oimIdx`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderitems`
+--
+
+LOCK TABLES `orderitems` WRITE;
+/*!40000 ALTER TABLE `orderitems` DISABLE KEYS */;
+INSERT INTO `orderitems` VALUES (1,1,1,1,1000),(2,2,1,1,3000),(3,3,1,2,2000),(4,4,1,1,3000),(5,8,1,1,1000),(6,1,2,1,1000),(7,2,2,1,3000),(8,3,2,1,1000),(9,4,2,1,3000),(10,14,3,1,1000),(11,10,3,1,1000),(12,12,3,2,2000),(13,15,3,1,1000);
+/*!40000 ALTER TABLE `orderitems` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `ordIdx` int NOT NULL AUTO_INCREMENT,
+  `orderDt` varchar(255) NOT NULL,
+  `orderPrice` int NOT NULL,
+  `orderNum` int NOT NULL,
+  `orderState` int NOT NULL,
+  PRIMARY KEY (`ordIdx`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'2025-02-21 20:34',10000,1,1),(2,'2025-02-21 20:38',8000,2,1),(3,'2025-02-21 20:39',5000,3,1);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `prdIdx` int NOT NULL AUTO_INCREMENT,
+  `prdName` varchar(45) NOT NULL,
+  `prdPrice` varchar(45) NOT NULL,
+  PRIMARY KEY (`prdIdx`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'꽈배기','1000'),(2,'꽈배기(3개)','3000'),(3,'찹쌀도너츠','1000'),(4,'찹쌀도너츠(3개)','3000'),(5,'도너츠','1000'),(6,'핫도그','1000'),(7,'아이스아메리카노','1000'),(8,'따뜻한아메리카노','1000'),(9,'석류아이스티','1000'),(10,'매실아이스티','1000'),(11,'블루베리아이스티','1000'),(12,'복숭아아이스티','1000'),(13,'콜라','1000'),(14,'사이다','1000'),(15,'환타','1000');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-21 20:43:59
